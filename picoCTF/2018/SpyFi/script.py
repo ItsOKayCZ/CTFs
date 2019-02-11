f16f7b965179360f032e23ec0bb3edabac04df22
Olda Krup <djslimelp@gmail.com>
2019-02-11 09:45:05 +0000

    new

--- picoCTF/2018/SpyFi/script.py
+++ picoCTF/2018/SpyFi/script.py
@@ -13,49 +13,58 @@ Same block:
 """
 
 length = 16
-for i in range(1, 20):
-	s = remote("2018shell3.picoctf.com", 31123)
-
-	s.recvuntil("Please enter your situation report: ")
-
-	padding = "B"*i + "A"*length*2
-	print "[#] Padding: {0}".format(padding)
-	print "[#] Count: {0}".format(i)
-	s.sendline(padding)
-
-	cipher = re.findall("."*length*2, s.recvall())
-
-	for i in range(0, len(cipher)):
-		for j in range(i + 1, len(cipher)):
-			if(cipher[i] == cipher[j]):
-				print "----------------------------"
-				print "Found same block {0} {1}".format(i, j)
-				print "----------------------------"
-
-	print "{0}\n".format(" ".join(cipher))
-
-# blockOffset = 11
-# myBlock = 4
-# while True:
-
-# 	# Get the current block that i need to decipher
+# for i in range(1, 20):
 # 	s = remote("2018shell3.picoctf.com", 31123)
+
 # 	s.recvuntil("Please enter your situation report: ")
 
-# 	padding = "B"*blockOffset + "A"*(length - 1)
+# 	padding = "B"*i + "A"*length*2
+# 	print "[#] Padding: {0}".format(padding)
+# 	print "[#] Count: {0}".format(i)
 # 	s.sendline(padding)
 
-# 	cipher = re.findall("."*length*2, s.recvall())[myBlock]
-# 	print cipher
+# 	cipher = re.findall("."*length*2, s.recvall())
 
-# 	for i in range(32, 126):
-# 		s = remote("2018shell3.picoctf.com", 31123)
+# 	for i in range(0, len(cipher)):
+# 		for j in range(i + 1, len(cipher)):
+# 			if(cipher[i] == cipher[j]):
+# 				print "----------------------------"
+# 				print "Found same block {0} {1}".format(i, j)
+# 				print "----------------------------"
 
-# 		s.recvuntil("Please enter your situation report: ")
+# 	print "{0}\n".format(" ".join(cipher))
 
-# 		padding = "B"*blockOffset + "A"*(length + 1) + chr(i)
-# 		print "[#] Padding: {0}".format(padding)
-# 		s.sendline(padding)
+blockOffset = 11
+myBlock = 4
 
-# 		cipher = re.findall("."*length*2, s.recvall())[myBlock]
-# 		print cipher
+output = ""
+while True:
+
+	# Get the current block that i need to decipher
+	s = remote("2018shell3.picoctf.com", 31123)
+	s.recvuntil("Please enter your situation report: ")
+
+	padding = "B"*blockOffset + "A"*(length - 1)
+	s.sendline(padding)
+
+	bruteBlock = re.findall("."*length*2, s.recvall())[myBlock]
+	print "[#] Trying to bruteforce: {}".format(bruteBlock)
+
+	for i in range(32, 126):
+		s = remote("2018shell3.picoctf.com", 31123)
+
+		s.recvuntil("Please enter your situation report: ")
+
+		padding = "B"*blockOffset + "A"*(length - 1) + chr(i)
+		print "[#] Padding: {0}".format(padding)
+		print "[#] Same block: {0}".format(bruteBlock)
+		s.sendline(padding)
+
+		cipher = re.findall("."*length*2, s.recvall())[myBlock]
+		if(cipher == bruteBlock):
+			print "--------------------"
+			print "Found char: {}".format(chr(i))
+			print "--------------------"
+			break
+		
+		print "[#] Bruteforce: {}\n".format(cipher)
