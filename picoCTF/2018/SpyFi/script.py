3668ed3620df03e6b3eefda318101ff5d19ee99d
ItsOKayCZ <ItsOKayCZ@gmail.com>
2019-02-15 11:57:28 +0100

    New

--- picoCTF/2018/SpyFi/script.py
+++ picoCTF/2018/SpyFi/script.py
@@ -35,36 +35,3 @@ length = 16
 # 	print "{0}\n".format(" ".join(cipher))
 
 blockOffset = 11
-myBlock = 4
-
-output = ""
-while True:
-
-	# Get the current block that i need to decipher
-	s = remote("2018shell3.picoctf.com", 31123)
-	s.recvuntil("Please enter your situation report: ")
-
-	padding = "B"*blockOffset + "A"*(length - 1)
-	s.sendline(padding)
-
-	bruteBlock = re.findall("."*length*2, s.recvall())[myBlock]
-	print "[#] Trying to bruteforce: {}".format(bruteBlock)
-
-	for i in range(32, 126):
-		s = remote("2018shell3.picoctf.com", 31123)
-
-		s.recvuntil("Please enter your situation report: ")
-
-		padding = "B"*blockOffset + "A"*(length - 1) + chr(i)
-		print "[#] Padding: {0}".format(padding)
-		print "[#] Same block: {0}".format(bruteBlock)
-		s.sendline(padding)
-
-		cipher = re.findall("."*length*2, s.recvall())[myBlock]
-		if(cipher == bruteBlock):
-			print "--------------------"
-			print "Found char: {}".format(chr(i))
-			print "--------------------"
-			break
-		
-		print "[#] Bruteforce: {}\n".format(cipher)
